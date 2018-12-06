63ffeb0b73d287dc36c5ec981455536a95bfad61
jandreas@ucsc.edu <Juan Andreas>
2018-12-05 22:29:44 -0800

    working on test cases and put node

--- dsproj_app/api_functions/view/put.py
+++ dsproj_app/api_functions/view/put.py
@@ -21,7 +21,7 @@ def put_handling(request, details):
     for curr_ip_port in all_ips.split(","):
         if curr_ip_port != new_ip_port:
             requests.put("http://"+new_ip_port+"/add-view",
-                         data={'view': environ.get("VIEW")})
+                        data={'view': environ.get("VIEW")})
             break
 
     if new_ip_port not in environ["VIEW"]:
@@ -32,13 +32,13 @@ def put_handling(request, details):
 
         # inefficient broadcast. change later
         for curr_ip_port in all_ips.split(","):
-            current_ip_is_not_new_ip = (curr_ip_port != new_ip_port)
+            current_ip_is_not_new_ip and current_ip_is_not_client_ip:
+                requests.put("http://"+curr_ip_port+"/view",
+                            data={'ip_port': new_ip_port})
+        clock.push()urrent_ip_is_not_new_ip = (curr_ip_port != new_ip_port)
             current_ip_is_not_client_ip = (
                 curr_ip_port != environ.get("IP_PORT"))
-            if current_ip_is_not_new_ip and current_ip_is_not_client_ip:
-                requests.put("http://"+curr_ip_port+"/view",
-                             data={'ip_port': new_ip_port})
-        clock.push()
+            if c
         resultmsg = "Success"
         msg = "Successfully added " + new_ip_port + " to view"
         statuscode = 200
